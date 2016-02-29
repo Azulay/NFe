a11225c0c7ebafdfc139954d260e538d7f3e1f7b
Felipe Barros <felipebarros.ti@gmail.com>
2016-02-28 23:15:09 -0300

    Adiciona teste NFeTest

--- src/file/NFeTest.java
+++ src/file/NFeTest.java
@@ -10,8 +10,25 @@ import org.junit.Test;
 public class NFeTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_set_content_from_file() throws IOException {
+		
+		NFe nfe = new NFe();
+		
+		nfe.setContentFromFile("D:\\java\\workspace\\NFe\\src\\file\\teste_01.xml");		
+		
+		assertNotNull("failed - content must contain something", nfe.getContent());
+		
 	}
 
+	
+	@Test
+	public void test_set_null_content_from_file() throws IOException {
+		
+		NFe nfe = new NFe();
+		
+		nfe.setContentFromFile("D:\\java\\workspace\\NFe\\src\\file\\teste_02.xml");		
+		
+		assertNull("failed - content must contain nothing", nfe.getContent());
+		
+	}
 }
