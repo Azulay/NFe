8e714c11b0296fbccc7f2c541939e088184a5b94
Felipe Barros <felipebarros.ti@gmail.com>
2016-03-02 23:58:59 -0300

    Corrige problemas de Assinatura e passa a gravar <Signature> no proprio arquivo

--- src/file/NFeTest.java
+++ src/file/NFeTest.java
@@ -5,30 +5,33 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.nio.file.Path;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class NFeTest {
 
+	NFe nfeLCA = null;
+	NFe nfeNull = null;
+	
+	@Before
+	public void setUp() throws IOException{
+		this.nfeLCA = new NFe();
+		
+		this.nfeLCA.setContentFromFile("D:\\dev\\resources\\xml\\NFE_LCA_001.xml");
+		
+		this.nfeNull = new NFe();
+		
+		this.nfeNull.setContentFromFile("D:\\dev\\resources\\xml\\xml_nulo.xml");	
+	}
+	
 	@Test
-	public void test_set_content_from_file() throws IOException {
-		
-		NFe nfe = new NFe();
-		
-		nfe.setContentFromFile("D:\\java\\workspace\\NFe\\src\\file\\teste_01.xml");		
-		
-		assertNotNull("failed - content must contain something", nfe.getContent());
-		
+	public void test_set_content_from_file() throws IOException {		
+		assertNotNull("failed - content must contain something", nfeLCA.getContent());		
 	}
 
 	
 	@Test
 	public void test_set_null_content_from_file() throws IOException {
-		
-		NFe nfe = new NFe();
-		
-		nfe.setContentFromFile("D:\\java\\workspace\\NFe\\src\\file\\teste_02.xml");		
-		
-		assertNull("failed - content must contain nothing", nfe.getContent());
-		
+		assertNull("failed - content must contain nothing", this.nfeNull.getContent());
 	}
 }
